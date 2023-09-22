9951a5a08c865c6ef37872ba08de28b359a38d82
kipkoechke <chepkwonyke1@gmail.com>
2023-09-22 07:35:35 +0300

    Multiple if statements

---
+++ Day 3/Multiple if statements/roller_coster.py
@@ -0,0 +1,23 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height: "))
+bill = 0
+
+if height > 120:
+    print("Wow! You can ride.")
+    age = int(input("What is your age?"))
+    if age < 12:
+        bill = 5
+        print("Child tickets are $5")
+    elif age < 18:
+        bill = 7
+        print("Youth tickets are $7")
+    else:
+        bill = 12
+        print("Adult tickets are $12")
+
+    want_photo = input("Want to take a photo? Y or N.")
+    if want_photo == "Y":
+        bill += 3
+    print(f"Total bill is ${bill}")
+else:
+    print("Sorry! You can't ride.")
