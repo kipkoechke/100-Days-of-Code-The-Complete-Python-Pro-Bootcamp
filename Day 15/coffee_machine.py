9f258054cb5e21685474eb5259b6c4d48b52cf0f
kipkoechke <chepkwonyke1@gmail.com>
2023-10-08 06:37:37 +0300

    Made coffe

--- Day 15/coffee_machine.py
+++ Day 15/coffee_machine.py
@@ -52,6 +52,25 @@ def process_coins():
     return total
 
 
+def is_transaction_successful(money_received, drink_cost):
+    if money_received > drink_cost:
+        change = round(money_received - drink_cost)
+        print(f"Here is ${change} dollars in change.")
+        global profit
+        profit += drink_cost
+        return True
+    else:
+        print("Sorry that's not enough money. Money refunded.")
+        return False
+
+
+def make_coffee(drink_name, order_ingredients):
+    """Deduct the required ingredients from the resources"""
+    for item in order_ingredients:
+        resources[item] -= order_ingredients[item]
+    print(f"Here is your {drink_name}.")
+
+
 is_on = True
 
 
@@ -67,6 +86,6 @@ while is_on:
     else:
         drink = MENU[choice]
         if is_resource_sufficient(drink["ingredients"]):
-            process_coins()
-
-
+            payment = process_coins()
+            if is_transaction_successful(payment, drink["cost"]):
+                make_coffee(choice, drink["ingredients"])
