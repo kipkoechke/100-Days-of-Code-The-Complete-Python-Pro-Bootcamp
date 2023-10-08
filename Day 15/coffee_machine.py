89eeb46f4ec927accdade8af4562600636226852
kipkoechke <chepkwonyke1@gmail.com>
2023-10-08 05:54:32 +0300

    Turned off the coffee machine

--- Day 15/coffee_machine.py
+++ Day 15/coffee_machine.py
@@ -30,7 +30,11 @@ resources = {
     "coffee": 100,
 }
 
-choice = input("What would you like? (espresso/latte/cappuccino): ")
+is_on = True
+while is_on:
+    choice = input("What would you like? (espresso/latte/cappuccino): ")
+    if choice == "off":
+        is_on = False
 
 
 
