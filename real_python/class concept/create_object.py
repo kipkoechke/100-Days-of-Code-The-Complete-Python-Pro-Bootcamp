e81547daec87eec344ba4de2b98118c7e31c92da
kipkoechke <chepkwonyke1@gmail.com>
2023-10-10 23:44:42 +0300

    Accessed attributes and methods

--- real_python/class concept/create_object.py
+++ real_python/class concept/create_object.py
@@ -3,5 +3,7 @@ from circle import Circle
 circle_1 = Circle(42)
 circle_2 = Circle(7)
 
-print(circle_1)
-print(circle_2)
+print(circle_1.radius)
+print(circle_1.calculate_area())
+print(circle_2.radius)
+print(circle_2.calculate_area)
