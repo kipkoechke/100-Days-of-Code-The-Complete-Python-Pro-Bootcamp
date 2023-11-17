278d2d89383c02ae6ba17675bc515a63e7194dc1
kipkoechke <chepkwonyke1@gmail.com>
2023-11-18 01:52:00 +0300

    Returning Multiple Values From a Function

--- real_python/tuples_lists_dictionaries/tuples.py
+++ real_python/tuples_lists_dictionaries/tuples.py
@@ -17,3 +17,11 @@ print(name[2:4])
 # Tuples Are Iterable
 for n in name:
     print(n.upper())
+
+
+# Returning Multiple Values From a Function
+def adder_subtractor(num1, num2):
+    return (num1 + num2, num1 - num2)
+
+
+print(adder_subtractor(3, 2))
