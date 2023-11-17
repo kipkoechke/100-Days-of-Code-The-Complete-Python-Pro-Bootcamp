3431984dc5fcde5fd23f8d0dc8559369fa55790a
kipkoechke <chepkwonyke1@gmail.com>
2023-11-18 00:35:21 +0300

    Splitted amount between people

--- real_python/functions_and_loops/for_loop.py
+++ real_python/functions_and_loops/for_loop.py
@@ -7,3 +7,12 @@ print("")
 
 for n in range(10, 20):
     print(n * n)
+
+print("")
+print("================")
+print("")
+
+amount = float(input("Enter an amount: "))
+
+for num_people in range(2, 6):
+    print(f"{num_people} people: ${amount / num_people:.2f} each")
