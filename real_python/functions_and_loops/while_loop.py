04c0fc23885c2171aab45cd81b65ffb980644e83
kipkoechke <chepkwonyke1@gmail.com>
2023-11-18 00:24:42 +0300

    Checked for user input meeting certain criteria

--- real_python/functions_and_loops/while_loop.py
+++ real_python/functions_and_loops/while_loop.py
@@ -2,3 +2,11 @@ n = 1
 while n < 5:
     print(n)
     n = n + 1
+
+print("=============================")
+
+num = float(input("Enter a positive number: "))
+
+while num <= 0:
+    print("That's not a positive number")
+    num = float(input("Enter a positive number: "))
