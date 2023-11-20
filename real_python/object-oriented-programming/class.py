8a0d704985423d57bc2f481097c6d16fda6aae85
kipkoechke <chepkwonyke1@gmail.com>
2023-11-20 13:03:28 +0300

    Completed exercise 2

--- real_python/object-oriented-programming/class.py
+++ real_python/object-oriented-programming/class.py
@@ -34,3 +34,18 @@ print("********************************")
 print(miles.speak("Woof Woof"))
 print(miles.speak("Bow Bow"))
 print(miles)
+
+
+class Car:
+    def __init__(self, color, mileage):
+        self.color = color
+        self.mileage = mileage
+
+    def __str__(self):
+        return f"The {self.color} car has {self.mileage:,} miles"
+
+
+blue = Car("blue", 20000)
+red = Car("red", 30000)
+print(blue)
+print(red)
