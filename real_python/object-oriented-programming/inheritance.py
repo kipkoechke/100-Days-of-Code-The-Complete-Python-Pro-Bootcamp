ad7759a6881390dde050098d2047c820f16e973e
kipkoechke <chepkwonyke1@gmail.com>
2023-11-20 13:27:12 +0300

    Worked on child classes

--- real_python/object-oriented-programming/inheritance.py
+++ real_python/object-oriented-programming/inheritance.py
@@ -1,10 +1,9 @@
 class Dog:
     species = "Canis familiaris"
 
-    def __init__(self, name, age, breed):
+    def __init__(self, name, age):
         self.name = name
         self.age = age
-        self.breed = breed
 
     def __str__(self):
         return f"{self.name} is {self.age} years old"
@@ -13,15 +12,24 @@ class Dog:
         return f"{self.name} says {sound}"
 
 
-miles = Dog("Miles", 4, "Jack Russell Terrier")
-buddy = Dog("Buddy", 9, "Dachshund")
-jack = Dog("Jack", 3, "Bulldog")
-jim = Dog("Jim", 5, "Bulldog")
-print(miles)
-print(miles.speak("hooo"))
+class JackRussellTerrier(Dog):
+    pass
+
+
+class Dachshund(Dog):
+    pass
+
+
+class Bulldog(Dog):
+    pass
+
+
+miles = JackRussellTerrier("Miles", 4)
+buddy = Dachshund("Buddy", 9)
+jack = Bulldog("Jack", 3)
+jim = Bulldog("Jim", 5)
+
+print(isinstance(miles, Dog))
 print(buddy)
-print(buddy.speak("Yap"))
 print(jack)
-print(jack.speak("Woof"))
 print(jim)
-print(jim.speak("Woof"))
