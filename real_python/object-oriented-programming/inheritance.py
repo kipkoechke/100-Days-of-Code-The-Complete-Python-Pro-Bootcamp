e2beff8a7ddcc64251f5702a1fe7efda5a69cc8f
kipkoechke <chepkwonyke1@gmail.com>
2023-11-20 13:14:35 +0300

    Created a dog class

---
+++ real_python/object-oriented-programming/inheritance.py
@@ -0,0 +1,27 @@
+class Dog:
+    species = "Canis familiaris"
+
+    def __init__(self, name, age, breed):
+        self.name = name
+        self.age = age
+        self.breed = breed
+
+    def __str__(self):
+        return f"{self.name} is {self.age} years old"
+
+    def speak(self, sound):
+        return f"{self.name} says {sound}"
+
+
+miles = Dog("Miles", 4, "Jack Russell Terrier")
+buddy = Dog("Buddy", 9, "Dachshund")
+jack = Dog("Jack", 3, "Bulldog")
+jim = Dog("Jim", 5, "Bulldog")
+print(miles)
+print(miles.speak("hooo"))
+print(buddy)
+print(buddy.speak("Yap"))
+print(jack)
+print(jack.speak("Woof"))
+print(jim)
+print(jim.speak("Woof"))
