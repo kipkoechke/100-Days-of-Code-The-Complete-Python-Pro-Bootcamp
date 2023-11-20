8c24e05b6f79a943654d9c6cb2b67b064f6b2d5e
kipkoechke <chepkwonyke1@gmail.com>
2023-11-20 19:35:54 +0300

    Using readline method

--- real_python/file_input_and_output/read.py
+++ real_python/file_input_and_output/read.py
@@ -3,3 +3,13 @@ input_file = open("hello.txt", "r")
 for line in input_file.readlines():
     print(line, end="")
 input_file.close()
+
+print("\n\n********using readline()*********")
+
+input_file = open("hello.txt", "r")
+
+line = input_file.readline()
+while line != "":
+    print(line, end="")
+    line = input_file.readline()
+input_file.close()
