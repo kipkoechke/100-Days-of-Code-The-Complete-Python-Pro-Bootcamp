37608c481fae0e5dc3a6c3fecab3eb09296dc01d
kipkoechke <chepkwonyke1@gmail.com>
2024-02-18 19:56:18 +0300

    Generating names

--- real_python/numbers_and_math/float.py
+++ real_python/numbers_and_math/float.py
@@ -1,11 +1,6 @@
-float1 = 1000000.0
-float2 = 1_000_000.0
-float3 = 1e6
+#!/usr/bin/python3
+contact_names = [f"Betford {i:04d}" for i in range(1, 1000000)]
 
-print(float1)
-print(float2)
-print(float3)
-
-print(type(float1))
-print(type(float2))
-print(type(float3))
+# Print the entire list
+for name in contact_names:
+    print(name)
