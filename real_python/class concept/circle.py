4287bffa1b16336c7dfe613f8cb8ed6202e04921
kipkoechke <chepkwonyke1@gmail.com>
2024-05-04 12:39:56 +0300

    Chore: Added doc strings

--- real_python/class concept/circle.py
+++ real_python/class concept/circle.py
@@ -2,8 +2,22 @@ import math
 
 class Circle:
     def __init__(self, radius):
+        """
+        Initializes a Circle object with the given radius.
+
+        Parameters:
+            radius (int or float): The radius of the circle.
+
+        Returns:
+            None
+        """
         self.radius = radius
 
-
     def calculate_area(self):
+        """
+        Calculates the area of a circle based on the given radius.
+
+        Returns:
+            float: The calculated area of the circle.
+        """
         return round(math.pi * self.radius ** 2, 2)
