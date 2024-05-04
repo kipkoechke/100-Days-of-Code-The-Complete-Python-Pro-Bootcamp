c8cae891192792341eb50dd1835b1401a138c26a
kipkoechke <chepkwonyke1@gmail.com>
2024-05-04 14:28:16 +0300

    Chore: Refactored code

--- real_python/object-oriented-programming/class.py
+++ real_python/object-oriented-programming/class.py
@@ -3,15 +3,40 @@ class Dog:
     species = "Canis familiaris"
 
     def __init__(self, name, age):
+        """
+        Initializes a new instance of the class with the given name and age.
+
+        Parameters:
+            name (str): The name of the object.
+            age (int): The age of the object.
+        """
         self.name = name
         self.age = age
 
     # Instance method
     def __str__(self):
+        """
+        Returns a string representation of the object.
+
+        This method is part of the special method overloading in Python. It is called when the `str()` function is used on an instance of the class.
+
+        Returns:
+            str: A string representation of the object, showing the name and age of the dog.
+        """
         return f"{self.name} is {self.age} years old"
 
     # Another instance method
     def speak(self, sound):
+        """
+        A method that returns a string indicating the dog's name and the sound it makes.
+
+        Parameters:
+            self: The instance of the class.
+            sound (str): The sound the dog makes.
+
+        Returns:
+            str: A string indicating the name of the dog and the sound it makes.
+        """
         return f"{self.name} says {sound}"
 
 
